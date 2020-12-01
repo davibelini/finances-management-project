122c22d5ea1602bcaf0759a6c61cb3f21d861da0
davibelini <dev.dado.belini@gmail.com>
2020-12-01 18:29:16 -0300

    tried to connect to mysql(errors and bugs)

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const app = express()
 const port = 3000 || process.env.PORT
+require('dotenv').config()
 const mysql = require('mysql')
 
 app.use(express.static('./public'))
 app.set('view engine', 'ejs')
+
+// MySQL config
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.MYSQL_CONNECTION_PASSWORD,
+  port: port
+});
+
+connection.connect((err) => {
+  if(err) {
+    return console.log(err)
+  }
+  console.log("Connected!")
+})
 
 app.get('/', (req, res) => {
   res.redirect('/login')
@@ -13,7 +29,7 @@ app.get('/login', (req, res) => {
   res.render(`login`)
 })
 app.get('/you', (req, res) => {
-  res.render(`you`, {req: req})
+  res.render('you', {req: req})
 })
 
 app.listen(port)
